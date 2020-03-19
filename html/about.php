1efdb4d37df22ed2750cdcd4942c82404c153a4b
chashtag <you@example.com>
2020-03-19 14:22:39 -0400

    Add link to github

--- html/about.php
+++ html/about.php
@@ -6,5 +6,6 @@
     <p class="lead">
     COVID utilizies a state of the art, device config parser to make observations about the security configurations of many different device types such as routers, firewalls, and switches of a network infrastructure.
     </p>
+    <p><a href="https://github.com/chashtag/covid_gamebox">Want to make your own COVID machine?</a></p>
   </main>
 <?php include 'templates/footer.php';?>
